f416e190a81bf3cde8fd8b3b5bbff6f950461408
vsvsgg594 <vsvsgg594@gmail.com>
2025-03-04 17:56:23 +0530

    fix bugs

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
-    host: '0.0.0.0',  // Allows access from any network
-    port: 5173,        // Change this if another process is using 5173
-    strictPort: true,  // Ensures Vite doesn't switch to another port
+    allowedHosts: ['weatherapi-zybd.onrender.com1'], // Allow this specific host
   },
 });
